783d4257e27ae56027d8d4bff8b09aff92e0b3f3
hfyfe <henryfyfe@gmail.com>
2016-11-06 20:28:03 +0000

    changes

--- take_pic.py
+++ take_pic.py
@@ -6,12 +6,12 @@ def TakePictures(directory,numbers):
     camera = PiCamera()
     camera.start_preview()
     sleep(2)
-    for i in range(numbers)
+    for i in range(int(numbers)):
         camera.capture(directory + '/image%s.jpg' % i)
-        print 'taking picture #' +i
+        print 'taking picture #' +str(i)
         sleep(1)
 
     camera.stop_preview()
     return ;
 
-TakePictures(sys.argv[1],sys.argv[1])
+TakePictures(sys.argv[1],sys.argv[2])
