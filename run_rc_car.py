0ec66c26ce147a872cc3d805ce4a191bb50e1311
Henry Fyfe <henryfyfe@gmail.com>
2016-11-07 12:25:30 -0600

    convert PiCar to class

--- run_rc_car.py
+++ run_rc_car.py
@@ -1,77 +1,78 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-def initialize():
-    GPIO.setmode(GPIO.BOARD)
-    pins = {'left'     : 11,
-            'right'    : 18,
-            'forward'  : 12,
-            'backward' : 16 }
-    GPIO.setmode(GPIO.BOARD)
-    for pin , pin_number in pins.iteritems()
-        GPIO.setup(pin_number,GPIO.OUT)
-    return;
+def PiCar():
 
-def stop():
-    for pin in pins.attributes
-        GPIO.output((pins[pin],0)
-    return;
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        self.pins = {'left'     : 11,
+                     'right'    : 18,
+                     'forward'  : 12,
+                     'backward' : 16 }
+        for pin_number in self.pins.itervalues():
+            GPIO.setup(pin_number,GPIO.OUT)
+        return;
 
-def go_forward(time):
-    stop()
-    GPIO.output(pins['forward'],1)
-    sleep(int(time))
-    GPIO.output(pins['forward'],0)
-    return;
+    def stop():
+        for pin_number in self.pins.itervalues():
+            GPIO.output((pins[pin],0)
+        return;
 
-def go_backward(time):
-    stop()
-    GPIO.output(pins['backward'],1)
-    sleep(int(time))
-    GPIO.output(pins['backward'],0)
-    return;
+    def go_forward(time):
+        stop()
+        GPIO.output(self.pins['forward'],1)
+        sleep(int(time))
+        GPIO.output(self.pins['forward'],0)
+        return;
 
-def go_forward_right(time):
-    stop()
-    GPIO.output(pins['right'],1)
-    GPIO.output(pins['forward'],1)
-    sleep(int(time))
-    GPIO.output(pins['forward'],0)
-    GPIO.output(pins['right'],0)
-    return;
+    def go_backward(time):
+        stop()
+        GPIO.output(self.pins['backward'],1)
+        sleep(int(time))
+        GPIO.output(self.pins['backward'],0)
+        return;
 
-def go_forward_left(time):
-    stop()
-    GPIO.output(pins['left'],1)
-    GPIO.output(pins['forward'],1)
-    sleep(int(time))
-    GPIO.output(pins['forward'],0)
-    GPIO.output(pins['left'],0)
-    return;
+    def go_forward_right(time):
+        stop()
+        GPIO.output(self.pins['right'],1)
+        GPIO.output(self.pins['forward'],1)
+        sleep(int(time))
+        GPIO.output(self.pins['forward'],0)
+        GPIO.output(self.pins['right'],0)
+        return;
 
-def go_backward_right(time):
-    stop()
-    GPIO.output(pins['right'],1)
-    GPIO.output(pins['backward'],1)
-    sleep(int(time))
-    GPIO.output(pins['backward'],0)
-    GPIO.output(pins['right'],0)
-    return;
+    def go_forward_left(time):
+        stop()
+        GPIO.output(self.pins['left'],1)
+        GPIO.output(self.pins['forward'],1)
+        sleep(int(time))
+        GPIO.output(self.pins['forward'],0)
+        GPIO.output(self.pins['left'],0)
+        return;
 
-def go_backward_left(time):
-    stop()
-    GPIO.output(pins['left'],1)
-    GPIO.output(pins['backward'],1)
-    sleep(int(time))
-    GPIO.output(pins['backward'],0)
-    GPIO.output(pins['left'],0)
-    return;
+    def go_backward_right(time):
+        stop()
+        GPIO.output(self.pins['right'],1)
+        GPIO.output(self.pins['backward'],1)
+        sleep(int(time))
+        GPIO.output(self.pins['backward'],0)
+        GPIO.output(self.pins['right'],0)
+        return;
+
+    def go_backward_left(time):
+        stop()
+        GPIO.output(self.pins['left'],1)
+        GPIO.output(self.pins['backward'],1)
+        sleep(int(time))
+        GPIO.output(self.pins['backward'],0)
+        GPIO.output(self.pins['left'],0)
+        return;
 
 
-initialize()
-go_forward(1)
-go_forward_right(1)
-go_backward_left(1)
-go_forward(2)
-go_backward(2)
-go_forward_right(5)
+picar = PiCar()
+picar.go_forward(1)
+picar.go_forward_right(1)
+picar.go_backward_left(1)
+picar.go_forward(2)
+picar.go_backward(2)
+picar.go_forward_right(5)
